ddf2722b15bdb41d03a76a629ba8be49054d9252
gabyrodrigues <gabyrlima.99@gmail.com>
2019-08-11 03:46:46 -0300

    carousel responsivo

---
+++ js/carousel.js
@@ -0,0 +1,37 @@
+$('.carousel').slick({
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: false,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.carousel-br').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
